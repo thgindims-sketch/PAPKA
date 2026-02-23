cc6b5913d3354385c093ed0d073c124008595361
Pud1nggg <thgindims@gmail.com>
2026-02-23 20:52:30 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    
    Changes not staged for commit:
    	modified:   bin/Debug/net10.0/PAPKA.dll
    	modified:   bin/Debug/net10.0/PAPKA.pdb
    	modified:   obj/Debug/net10.0/PAPKA.dll
    	modified:   obj/Debug/net10.0/PAPKA.pdb

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System; // Импорт библиотеки "System".
+
+namespace PAPKA // Создание именного пространства с названием папки, в которой находится на проект.
+{
+    class Program // Импортируем класс с названием нашего .cs проекта.
+    {
+        static void Main() // Не знпю что это.
+        {
+            int number = -10;
+            Console.WriteLine(number);
+            
+            Console.WriteLine("HMYR 1"); // }
+            Console.WriteLine("HMYR 2"); // } Вывод информации в консоль.
+            Console.WriteLine("HMYR 3"); // }
+            Console.Read(); // Консоль ожидает какой-нибудь ввод информации пользователя для завершения работы кода. aka получение данных
+        }
+    }
+}
